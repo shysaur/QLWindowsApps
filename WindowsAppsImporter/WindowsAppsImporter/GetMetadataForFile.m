d7b092373178ad70f203dc7f5343f1e806fde7b7
Daniele Cattaneo <daniele.cattaneo@alice.it>
2017-12-19 10:46:56 +0100

    Implement a spotlight importer that imports the version info.

---
+++ WindowsAppsImporter/WindowsAppsImporter/GetMetadataForFile.m
@@ -0,0 +1,91 @@
+//
+//  GetMetadataForFile.m
+//  WindowsAppsImporter
+//
+//  Created by Daniele Cattaneo on 16/12/17.
+//  Copyright © 2017 danielecattaneo. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+#import <Foundation/Foundation.h>
+#import "EIExeFile.h"
+#import "EIVersionInfo.h"
+
+
+Boolean GetMetadataForFile(void *thisInterface, CFMutableDictionaryRef attributes, CFStringRef contentTypeUTI, CFStringRef pathToFile);
+
+
+BOOL EIMetadataForFile(NSURL *url, NSMutableDictionary *attr)
+{
+  static NSSet *recognizedTags;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    recognizedTags = [NSSet setWithArray:@[
+      @"com_danielecattaneo_windowsappsimporter_companyname",
+      @"com_danielecattaneo_windowsappsimporter_filedescription",
+      @"com_danielecattaneo_windowsappsimporter_fileversion",
+      @"com_danielecattaneo_windowsappsimporter_internalname",
+      @"com_danielecattaneo_windowsappsimporter_legaltrademarks",
+      @"com_danielecattaneo_windowsappsimporter_originalfilename",
+      @"com_danielecattaneo_windowsappsimporter_privatebuild",
+      @"com_danielecattaneo_windowsappsimporter_productname"]];
+  });
+
+  EIExeFile *f = [[EIExeFile alloc] initWithExeFileURL:url];
+  if (!f)
+    return NO;
+  EIVersionInfo *vir = [f versionInfo];
+  if (!vir)
+    return NO;
+  
+  NSString *queryHeader = @"\\StringFileInfo\\*";
+  NSArray *resSrch = [vir querySubNodesUnder:queryHeader error:NULL];
+  if (!resSrch)
+    return NO;
+  
+  for (NSString *node in resSrch) {
+    NSString *vpath = [NSString stringWithFormat:@"%@\\%@", queryHeader, node];
+    NSString *value = [vir queryStringValue:vpath error:NULL];
+    if (!value)
+      continue;
+    
+    NSString *destkey;
+    if ([node isEqual:@"Comments"])
+      destkey = (NSString *)kMDItemComment;
+    else if ([node isEqual:@"LegalCopyright"])
+      destkey = (NSString *)kMDItemCopyright;
+    else {
+      NSString *test = [@"com_danielecattaneo_windowsappsimporter_" stringByAppendingString:[node lowercaseString]];
+      if ([recognizedTags containsObject:test])
+        destkey = test;
+    }
+    
+    if (destkey)
+      [attr setObject:value forKey:destkey];
+  }
+  
+  return YES;
+}
+
+
+//==============================================================================
+//
+//  Get metadata attributes from document files
+//
+//  The purpose of this function is to extract useful information from the
+//  file formats for your document, and set the values into the attribute
+//  dictionary for Spotlight to include.
+//
+//==============================================================================
+
+Boolean GetMetadataForFile(void *thisInterface, CFMutableDictionaryRef attributes, CFStringRef contentTypeUTI, CFStringRef pathToFile)
+{
+  Boolean res;
+  
+  @autoreleasepool {
+    NSMutableDictionary *attr = (__bridge NSMutableDictionary *)(attributes);
+    NSURL *url = [[NSURL alloc] initFileURLWithPath:(__bridge NSString * _Nonnull)(pathToFile)];
+    res = EIMetadataForFile(url, attr);
+  }
+  return res;
+}
